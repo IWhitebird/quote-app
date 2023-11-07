a877af8c2777ae6f387d7194bf4f76061fa43d6c
IWhitebird <patangeshreyas@gmail.com>
2023-11-08 04:06:01 +0530

    Update Tags.tsx

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -23,7 +23,12 @@ function Tags() {
   async function fetchTags() {
     setTagLoading(true);
     try {
-      const response = await axios.get("https://api.quotable.io/tags");
+      const response = await axios.get("https://api.quotable.io/tags" ,        
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
       setAllTags(response.data);
     } catch (error) {
       console.error("Error fetching tags:", error);
