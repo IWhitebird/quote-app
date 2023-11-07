6bc302e452e20bb0b585d2b7dd339d748d585f6f
IWhitebird <patangeshreyas@gmail.com>
2023-11-08 04:00:40 +0530

    Adding Logo & Title

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,36 +2,49 @@ import BookmarkIcon from "./BookmarkIcon";
 import { setBookmark } from "../slices/quoteSlice";
 import { useDispatch } from "react-redux";
 
-function Card({ quote , bookmarks }: { quote: any , bookmarks : any[]  }) {
+function Card({ quote, bookmarks }: { quote: any; bookmarks: any[] }) {
   const dispatch = useDispatch();
 
-  function bookmarksHandleV2(e : any) {
+  function bookmarksHandleV2(e: any) {
     e.preventDefault();
-    if(bookmarks.filter((bookmark : any) => bookmark._id === quote._id).length > 0) {
-      let newbookmark = bookmarks.filter((bookmark : any) => bookmark._id !== quote._id);
+    if (
+      bookmarks.filter((bookmark: any) => bookmark._id === quote._id).length > 0
+    ) {
+      let newbookmark = bookmarks.filter(
+        (bookmark: any) => bookmark._id !== quote._id
+      );
       dispatch(setBookmark(newbookmark));
+    } else {
+      dispatch(setBookmark([...bookmarks, quote]));
     }
-    else {
-      dispatch(setBookmark([...bookmarks , quote]));
-    } 
   }
-  
+
   return (
     <div className="w-full flex justify-center mb-12 ">
-      <div className="relative transition-all duration-200 ease-in-out hover:scale-105 w-[60%] bg-[#D05252] flex flex-col gap-8 p-4 rounded-lg text-white justify-center">
+      <div className="relative transition-all duration-200 ease-in-out hover:scale-[101%] w-[60%] bg-[#D05252] flex flex-col gap-8 p-4 rounded-lg text-white justify-center">
         <div className="text-3xl mt-5">{quote.content}</div>
         <div className="w-[90%] flex justify-between p-4 mx-auto">
-          <div className="italic">#{quote.tags.join("# ")}</div>
+          <div className="italic">
+            {quote.tags.map((tag: any, index: number) => (
+              <span key={index}>
+                #{tag}
+                {index < quote.tags.length - 1 && " "}
+              </span>
+            ))}
+          </div>
           <div className="font-bold">~ {quote.author}</div>
         </div>
 
-         <div className="absolute top-0 scale-125 right-10 z-10">
+        <div className="absolute top-0 scale-125 right-10 z-10">
           <button onClick={(e) => bookmarksHandleV2(e)}>
-            <BookmarkIcon defValue={bookmarks.filter((bookmark : any) => bookmark._id === quote._id).length > 0}/>
+            <BookmarkIcon
+              defValue={
+                bookmarks.filter((bookmark: any) => bookmark._id === quote._id)
+                  .length > 0
+              }
+            />
           </button>
-        </div> 
-
-
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from "react";
+import { useEffect , useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../reducer/reducer";
 import { setLoading, setQuote } from "../slices/quoteSlice";
 import Card from "../components/Card";
 import axios from "axios";
+import Tags from "../components/Tags";
 
 
 function Home() {
+
+  const [tagsModal, setTagsModal] = useState(false);
 
   const dispatch = useDispatch();
   let loading = useSelector((state: RootState) => state.quote.loading);
@@ -21,15 +24,18 @@ function Home() {
         dispatch(setLoading(false));
         return;
     } 
-    fetchQuote(tags);
+    fetchQuote();
   }, []); 
 
 
-  async function fetchQuote(tags: string[]) {
+  async function fetchQuote() {
     try {
       dispatch(setLoading(true));
-      let tagString: string = '?tags=' + tags.join(',');
-
+      console.log(tags)
+      let tagString: string = '?tags=' + tags.map((tag: any) => tag.name).join(',');
+      
+      let str = 'https://api.quotable.io/quotes/random' + tagString;
+      console.log(str)
       const response = await axios.get(
         'https://api.quotable.io/quotes/random' + tagString,
         {
@@ -48,29 +54,65 @@ function Home() {
 
 
   return (
-    <div className="w-full mt-10">
-      <div className="flex flex-col">
-        {!loading && quote && quote.content ? (
-            <Card bookmarks={bookmarks} quote={quote}  />
-        ) : (
-          <div className="w-[50%] mx-auto flex flex-col justify-center items-center gap-[50px]">
-            <div className="kinetic mt-10"></div>
-            <div className="mb-10 text-3xl text-white animate-pulse">
-              Fetching Quote ...
-            </div>
-          </div>
-        )}
+    <>
+      <div className="w-full mt-20">
+        <div className="flex flex-col">
+          <div className="min-h-[40vh]">
+            {!loading && quote && quote.content ? (
+                <Card bookmarks={bookmarks} quote={quote}  />
+            ) : (
+              <div className="w-[50%] mx-auto flex flex-col justify-center items-center gap-[50px]">
+                <div className="kinetic mt-10"></div>
+                <div className="mb-10 text-3xl text-white animate-pulse">
+                  Fetching Quote ...
+                </div>
+              </div>
+            )}
 
-        <div className="w-full flex justify-center">
-          <button
-            onClick={() => fetchQuote([])}
-            className="text-2xl rounded-full bg-green-600 w-[230px] h-[50px] hover:bg-green-400 hover:scale-110 hover:font-bold transition-all duration-200 ease-in-out"
-          >
-            New Quote
-          </button>
+          </div>
+
+
+          <div className="flex justify-center">
+            <div className="w-[300px] flex justify-center">
+              <button
+                onClick={fetchQuote}
+                className="text-2xl font-semibold rounded-full bg-green-600 w-[230px] h-[50px] hover:bg-yellow-400 hover:scale-110 hover:font-bold transition-all duration-200 ease-in-out"
+              >
+                New Quote
+              </button>
+            </div>
+
+            <div className="w-[300px] l flex justify-center">
+              <button
+                onClick={() => setTagsModal(true)}
+                className="text-2xl font-semibold rounded-full text-black bg-white w-[170px] h-[50px] hover:bg-black hover:text-white hover:scale-110 hover:font-bold transition-all duration-200 ease-in-out"
+              >
+                Tags
+              </button>
+            </div>
+
+          </div>
+
+
+
         </div>
       </div>
-    </div>
+      {
+      tagsModal && (
+        <div className="z-[100] fixed top-0 left-0 flex items-center justify-center w-screen h-screen bg-opacity-70 bg-black">
+          <div className="relative w-[80%] h-[80%] figma-bg overflow-x-auto  rounded-2xl">
+            <Tags />
+          </div>
+          <div onClick={() => setTagsModal(!tagsModal)} className="absolute z-[110] top-7 right-10 text-white text-5xl transition-all
+          duration-500 ease-in-out hover:scale-125 hover:rotate-180 font-bold cursor-pointer">
+            &times;
+          </div>
+        </div>
+      )
+    }
+              
+    </>
+
   );
 }
 
